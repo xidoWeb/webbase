8847a8e59c78a44ce83e1a845f5633b56dc25465
xido <eriver2@gmail.com>
2017-08-04 13:55:21 +0900

    accordion -5

--- assets/e_step_04_jquery/js/src/accordion_01.js
+++ assets/e_step_04_jquery/js/src/accordion_01.js
@@ -2,47 +2,49 @@
 
 (function($){
 // ==========================================
-	/*  jquery 아코디언 
+/*  jquery 아코디언 
 	* 1. 선택 개체 숨기기 ( hide() )
 	* 2. 선택 되지 않은 다른 개체 사라지게 만들기
 	*   2-1. 현재값에서 높이가 0이 되도록 animate
 	*   2-2. 0으로 만들어지면 hide()
 	* 3. 선택 개체 바로 뒤 요소 보이게
 	*   3-1. show() -> height:0
-	*   3-2. 원래 가져야하는 높이 값으로 설정 
-	*/
-
-	// $('dt').on('click',function(e){
-// 	console.log($(this).next('dd').index());
-// });
+*   3-2. 원래 가져야하는 높이 값으로 설정 
+*/
 
 
+ var addH = $('.add_h');
+ var addH_dt = addH.find('dt');
+ var addH_dd = addH.find('dd');
+ 
+ addH_dd.hide(); // display:none
 
-  $('.add_h').find('dd').hide();
-  $('dt').on('click',function(e){
-  	e.preventDefault();
+ addH_dt.on('click',function(e){
+	e.preventDefault();
 
-  	var _$this = $(this);
-  	var nextDd = _$this.next('dd');
+	var _$this = $(this);
+	var nextDd = _$this.next('dd');
+	var ddBro = nextDd.siblings('dd');
+// 클릭시 선택된 dd를 제외한 나머지 숨김처리
+	var formAni = function(evt){
+		evt.animate({height:0, padding:0},function(){
+		  		evt.css({display:'none'});
+		}); 
+	}; // fomeAni()
 
-  	// $('dd').not( $(this).next('dd') )
-  	nextDd.siblings('dd')
-  	      .animate({height:0, padding:0},function(){
-				  		$(this).css({display:'none'});
-				  	}); 
+// 숨겨진 dd를 보이게 만들기
+	var formView = function(evt){
+		evt.css({display:'block', padding:0, height:0, overflow:'hidden'})
+	     .animate({height:'200px', padding:'0.5em'});
+	}; // formView()
+  
+	formAni(ddBro);
 
-  	var ddDp = nextDd.css('display');
-  	 if(ddDp == 'none'){
-
-	  	nextDd.css({display:'block', padding:0, height:0, overflow:'hidden'})
-	  	      .animate({height:'200px', padding:'0.5em'});
-  	 }else{
-
-  	 	nextDd.animate({height:0, padding:0},function(){
-				  		$(this).css({display:'none'});
-				  	});
-  	 }// else      
-  });  // $('dt').on('click')
+// dd상태 확인하여 클릭시 처리
+	var ddDp = nextDd.css('display');
+	 (ddDp == 'none') ? formView(nextDd) : formAni(nextDd);
+	 
+});  // $('dt').on('click')
 
 
 // ==========================================
