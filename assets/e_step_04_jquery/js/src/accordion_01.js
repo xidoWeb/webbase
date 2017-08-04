c2dc2ec3745d2cdaf51078bce87991d8a4f69197
xido <eriver2@gmail.com>
2017-08-04 16:07:57 +0900

    accordion 각 내용 사이즈 설정

--- assets/e_step_04_jquery/js/src/accordion_01.js
+++ assets/e_step_04_jquery/js/src/accordion_01.js
@@ -11,20 +11,6 @@
 	*   3-1. show() -> height:0
 *   3-2. 원래 가져야하는 높이 값으로 설정 
 */
-/*
-	 // addH_dt.eq(3).css({background:'#f00'});
-	 // addH_dt.on('click',function(){
-	 // 	console.log(addH_dt.index( $(this) ));
-	 // });
-
-	 // var ddList = [];
-	 // var i = 0;
-	 // for(; i < addH_dd.length; i++){
-	 // 	ddList[i] = addH_dd.eq(i).height();
-	 // }
-	 // // console.log(ddList);
-*/
-
 
  var addH = $('.add_h');
  var addH_dt = addH.find('dt');
@@ -36,30 +22,29 @@
 	arr[i] = addH_dd.eq(i).height();
  }
  // -------------------------------------
-
- addH_dd.hide(); // display:none
- addH_dt.on('click',function(e){
-	e.preventDefault();
-	var _$this = $(this);
-	var nextDd = _$this.next('dd');
-	var ddBro = nextDd.siblings('dd');
-// 클릭시 선택된 dd를 제외한 나머지 숨김처리
-	var formAni = function(evt){
-		evt.animate({height:0, padding:0},function(){
-		  		evt.css({display:'none'});
-		}); 
-	}; // fomeAni()
-// 숨겨진 dd를 보이게 만들기
-	var formView = function(evt){
-		var j = addH_dt.index(_$this);
-		console.log(arr[j]);
-		evt.css({display:'block', padding:0, height:0, overflow:'hidden'})
-	     .animate({height:arr[j], padding:'0.5em'});
-	}; // formView()
-	formAni(ddBro);
-// dd상태 확인하여 클릭시 처리
-	var ddDp = nextDd.css('display');
-	 (ddDp == 'none') ? formView(nextDd) : formAni(nextDd);
+ addH_dd.hide(); 
+// dt클릭하여 기능 처리하기 
+	 addH_dt.on('click',function(e){
+		e.preventDefault();
+		var _$this = $(this);
+		var nextDd = _$this.next('dd');
+		var ddBro = nextDd.siblings('dd');
+	// 클릭시 선택된 dd를 제외한 나머지 숨김처리
+		var formAni = function(evt){
+			evt.animate({height:0, padding:0},function(){
+			  		evt.css({display:'none'});
+			}); 
+		}; // fomeAni()
+	// 숨겨진 dd를 보이게 만들기
+		var formView = function(evt){
+			var j = addH_dt.index(_$this);
+			evt.css({display:'block', padding:0, height:0, overflow:'hidden'})
+		     .animate({height:arr[j], padding:'0.5em'});
+		}; // formView()
+		formAni(ddBro);
+	// dd상태 확인하여 클릭시 처리
+		var ddDp = nextDd.css('display');
+		 (ddDp == 'none') ? formView(nextDd) : formAni(nextDd);
 });  // $('dt').on('click')
 
 
