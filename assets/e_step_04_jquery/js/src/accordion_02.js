5a12d7d33742d19f179943906ef8e0fceec0f240
xido <eriver2@gmail.com>
2017-08-07 11:44:41 +0900

    accordion_02 선택된것 나타나기

--- assets/e_step_04_jquery/js/src/accordion_02.js
+++ assets/e_step_04_jquery/js/src/accordion_02.js
@@ -8,19 +8,30 @@
 	var conDt    = conBoxDl.find('dt');
 	var conDd    = conBoxDl.find('dd');
 
-  var ddWidth = parseInt(conDd.eq(0).width()); 
-  var ddPdL =  parseInt(conDd.eq(0).css('paddingLeft'));
-  var ddPdR = parseInt(conDd.eq(0).css('paddingRight'));
-
-  // // 문자를 숫자(정수)로 변환  parseInt
-  // console.log('width: ', ddWidth);
-  // console.log('padding-left: ', ddPdL);
-  // console.log('padding-right: ', ddPdR);
-
   var ddFullWidth = conDd.eq(0).innerWidth();
-  console.log(ddFullWidth);
-  
-  
+  // console.log(ddFullWidth);
+
+  // dt클릭 이벤트
+  conDt.on('click', function(event) {
+  	event.preventDefault();
+  	var _$this = $(this);
+  	var thisNext = _$this.next('dd');
+  	
+  	// 선택된 dt뒤의 dd를 제외한 나머지 숨김처리
+  	thisNext.siblings('dd').animate({'width':0, 'overflow':'hidden'},function() {
+  		var _this = $(this);
+  		_this.css({'display':'none'});
+  	});
+
+  	// 선택된 dt뒤의 dd는 나타나게 만들기
+		thisNext.css({'display':'block', 'width':0,'overflow':'hidden'})
+						.animate({'width':ddFullWidth+'px'});	
+
+		// dl의 범위에서 넘치는 경우 (일부영역 숨김처리로 해결)				
+		conBoxDl.css({'overflow':'hidden'});
+  });
+
+
 // +++++++++++++++++++++++++++++++++
 })(this.jQuery);
 
