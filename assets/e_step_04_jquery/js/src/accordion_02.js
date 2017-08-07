3fb1267161482484e782ba5dd70e1592d0184a37
xido <eriver2@gmail.com>
2017-08-07 12:27:00 +0900

    accordion_02+ 드롭다운메뉴만들기

--- assets/e_step_04_jquery/js/src/accordion_02.js
+++ assets/e_step_04_jquery/js/src/accordion_02.js
@@ -2,7 +2,21 @@
 
 (function($){
 // +++++++++++++++++++++++++++++++++
+// drop down 메뉴
+	var gnb = $('#gnb');
+	var gnbLi = $('#gnb>ul>li');
+	var gnbOl = gnbLi.find('ol');
+// 마우스를 올렸을 경우( .on('mouseenter', function(){}); )
+	gnb.on('mouseenter', function(){
+		gnbOl.stop().slideDown();
+		});
+// 마우스를 벗어났을 경우( .on('mouseleave', function(){}); )
+	gnb.on('mouseleave', function(){
+		gnbOl.stop().delay(300).slideUp();
+		});
 
+
+//  아코디언 메뉴 만들기
 	var conBox   = $('#contentBox');
 	var conBoxDl = conBox.find('dl');
 	var conDt    = conBoxDl.find('dt');
@@ -23,9 +37,14 @@
   		_this.css({'display':'none'});
   	});
 
-  	// 선택된 dt뒤의 dd는 나타나게 만들기
-		thisNext.css({'display':'block', 'width':0,'overflow':'hidden'})
-						.animate({'width':ddFullWidth+'px'});	
+
+		// 조건을 확인하여 움직이지 않거나, 나타나게 만들기.
+		var nextDdDp = thisNext.css('display');
+		if(nextDdDp == 'none'){
+	  	// 선택된 dt뒤의 dd는 나타나게 만들기
+			thisNext.css({'display':'block', 'width':0,'overflow':'hidden'})
+							.animate({'width':ddFullWidth+'px'});	
+		}
 
 		// dl의 범위에서 넘치는 경우 (일부영역 숨김처리로 해결)				
 		conBoxDl.css({'overflow':'hidden'});
