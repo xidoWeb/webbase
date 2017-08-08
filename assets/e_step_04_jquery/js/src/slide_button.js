7bfe1d02e20372e56048275ccdab5053d9c3cf9e
xido <eriver2@gmail.com>
2017-08-08 14:43:00 +0900

    slide button rollover기능 및 문제 발생

--- assets/e_step_04_jquery/js/src/slide_button.js
+++ assets/e_step_04_jquery/js/src/slide_button.js
@@ -18,14 +18,9 @@ slide_btn.addClass('leftMove');
 // click시 .leftMove를 삭제
 slide_button.on('click',function(e){
 	e.preventDefault();
-
-	// hasClass()   : class이름의 존재 유무를 판단(true/false)
 	// is()					: class뿐 아니라 다른 내용도 판단가능
-
-	// var slide_left = parseInt(slide_btn.css('left'));
-	   // var slide_left =  slide_btn.hasClass('leftMove');
-	   var slide_left =  slide_btn.is('.leftMove');
-	   console.log(slide_left);
+   var slide_left =  slide_btn.is('.leftMove');
+   console.log(slide_left);
 
 	if( slide_left ){
 		slide_btn.removeClass('leftMove', 500);
@@ -36,15 +31,29 @@ slide_button.on('click',function(e){
 		slide_icon.removeClass('fa-arrow-left').addClass('fa-arrow-right');
 	  slide_button.find('span').text('열기');
 	}
-
 });
 
 
+var timed = 500;
 // 3. 버튼에 마우스 올렸을경우 .slide_btn이 살짝 나오기(약 10px정도)
+slide_button.on('mouseenter', function(event) {
+	event.preventDefault();
+	slide_btn.animate({'left':'-230px'},timed);
+});
+slide_button.on('mouseleave', function(event) {
+	event.preventDefault();
+	slide_btn.animate({'left':'-250px'},timed);
+});
 
-
+// 문제점: 마우스 클릭시 이동처리된 것이 에러를 발생
 
 })(this.jQuery);
+
+
+
+
+
+
 
 
 
