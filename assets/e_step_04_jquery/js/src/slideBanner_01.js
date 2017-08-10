6c5a544353a925dc9f749e2369db9bc62a594a57
xido <eriver2@gmail.com>
2017-08-10 11:32:31 +0900

    slidebanner -01 오른버튼 클릭시 이동 및 반복기능처리

--- assets/e_step_04_jquery/js/src/slideBanner_01.js
+++ assets/e_step_04_jquery/js/src/slideBanner_01.js
@@ -18,13 +18,23 @@
   	//li_last.prependTo(banner_group);				//   [자식으로처리될선택].내부앞에[부모]  담기
 
   // 2. 오른버튼을 클릭하면, 오른쪽 이미지가 보이게 이동
+  // 3. 여러번 클릭해도 계속 보이게 만들기
+  		
   	btn_r.on('click',function(e){
   		e.preventDefault();
-  		banner_group.animate({marginLeft:'-200%'}, timed);
+  		// 1 클릭시 한칸 왼쪽으로 이동 후
+  		banner_group.animate({marginLeft:-200+'%'}, timed, function(){
+  		// 2 맨앞의 요소를 마지막으로 이동(점프) 과 동시에
+  			var li_fr = banner_group.children('li').first();   // 이동된 변수
+	  			banner_group.append(li_fr); 			// [부모].내부 뒤에([자식])
+	  			// li_fr.appendTo(banner_group); 		// [자식].이 뒤에([부모])
 
+  		// 3 처음상태의 형태로 처리(단, 내부요소는 변경된 형태 그대로)
+	  			banner_group.css({marginLeft:-100+'%'});
+  		});
   	});
 
-  // 3. 여러번 클릭해도 계속 보이게 만들기
+
   // 4. 빠르게 클릭하면 생기는 메모리기능이 사라지게 되도록 처리
   // 5. 왼쪽 버튼을 클릭하면, 위와 동일한 반복기능 처리
 
