641c6ad704ba9c7060cc00061a0ab78ff152885e
xido <eriver2@gmail.com>
2017-08-10 11:15:36 +0900

    slide banner -01 클릭시 하나이동(샘플)

--- assets/e_step_04_jquery/js/src/slideBanner_01.js
+++ assets/e_step_04_jquery/js/src/slideBanner_01.js
@@ -18,6 +18,12 @@
   	//li_last.prependTo(banner_group);				//   [자식으로처리될선택].내부앞에[부모]  담기
 
   // 2. 오른버튼을 클릭하면, 오른쪽 이미지가 보이게 이동
+  	btn_r.on('click',function(e){
+  		e.preventDefault();
+  		banner_group.animate({marginLeft:'-200%'}, timed);
+
+  	});
+
   // 3. 여러번 클릭해도 계속 보이게 만들기
   // 4. 빠르게 클릭하면 생기는 메모리기능이 사라지게 되도록 처리
   // 5. 왼쪽 버튼을 클릭하면, 위와 동일한 반복기능 처리
