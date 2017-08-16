57348f694dfccb7a870b86c22e6041aee41786a1
xido <eriver2@gmail.com>
2017-08-16 10:34:51 +0900

    json_01

---
+++ assets/e_step_04_jquery/js/src/json_01.js
@@ -0,0 +1,64 @@
+//  json_01.js
+
+// JSON(javascript object Notation - 자바스크립트 객체 표현방법)
+// 과거 sql(db) -- 관계형db -table형태  : mySql, oracle, msSql, mariaDB........ 
+// 현재 sql(db) -- 빅데이터를 처리하거나, 좀더 복잡한 구조를 처리하기위해 개량: MongoDB
+
+// .html(); - 코드가 포함된 html형식의 값을 가져오거나, 담아줄때 사용 
+// .val();  - form형식의 값을 가져오거나 입력할때 사용
+// .text(); - 글자를 가져오거나, 글자를 변경
+// .append() | .appendTo()
+// .prepend() | .prependTo()
+
+(function($) {
+ var db = [
+ 		{'name': '이도형', 'gender':'male'},
+ 		{'name': '전혜빈', 'gender':'female'},
+ 		{'name': '김하영', 'gender':'female'},
+ 		{'name': '박새나', 'gender':'female'},
+ 		{'name': '서보람', 'gender':'female'},
+ 		{'name': '이수진', 'gender':'female'},
+ 		{'name': '이현준', 'gender':'female'},
+ 		{'name': '이단비', 'gender':'female'},
+ 		{'name': '이승희', 'gender':'female'},
+ 		{'name': '안영란', 'gender':'female'},
+ 	];
+// 	console.log(db[1].gender, db[0].name);
+
+ var headBox = $('#headBox');
+ var head_ul = headBox.children('ul');
+ var head_li = head_ul.children('li');
+ 
+ // head_li.eq(0).html('<span>').text(db[0].name);
+ // 위 형식을 기반으로 각 li에 내용을 담으시오!
+ 
+var i = 0;
+var li_length = head_li.length;  // 10;
+
+for(; i<li_length; i++){
+	head_li.eq(i).html('<span>');
+	head_li.eq(i).find('span').text(db[i].name);
+	// head_li.eq(i).html('<span>').find('span').text(db[i].name);
+}
+
+})(this.jQuery);
+
+// 네이버, 다음, 구글, w3schools,  darum.daum, nuli.navercorp
+// 위 홈페이지 주소를 찾아 링크와 함께 만드시오.
+
+// <ul>
+	// <li><a href="http://naver.com">네이버</a></li>
+
+	// .attr()
+
+
+
+
+
+
+
+
+
+
+
+
