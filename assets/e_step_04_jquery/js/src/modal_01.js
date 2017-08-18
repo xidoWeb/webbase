578128b4f9fd4f90de4c1df931cb761103770738
xido <eriver2@gmail.com>
2017-08-18 11:40:45 +0900

    modal_01 큰이미지 json처리

--- assets/e_step_04_jquery/js/src/modal_01.js
+++ assets/e_step_04_jquery/js/src/modal_01.js
@@ -27,14 +27,34 @@
 // ------------------------------
  var galleryBox = $('.gallery_list');
  var gallery_list = galleryBox.children('li');
+ // modal 큰이미지 json형태로 처리
+ var url = '../img/gallery/'
+ var modalData = [
+ 	{'src':'j01_big.jpg', 'alt':'큰이미지_1 설명'},
+ 	{'src':'j02_big.jpg', 'alt':'큰이미지_2 설명'},
+ 	{'src':'j03_big.jpg', 'alt':'큰이미지_3 설명'},
+ 	{'src':'j04_big.jpg', 'alt':'큰이미지_4 설명'},
+ 	{'src':'j05_big.jpg', 'alt':'큰이미지_5 설명'},
+ 	{'src':'j06_big.jpg', 'alt':'큰이미지_6 설명'},
+ 	{'src':'j07_big.jpg', 'alt':'큰이미지_7 설명'},
+ 	{'src':'j08_big.jpg', 'alt':'큰이미지_8 설명'},
+	];
 
+
+
+
+
+//------------------------------------
 // li클릭시 모달창 띄우기
  gallery_list.on('click',function(e){
  	e.preventDefault();
  	var _this = $(this);
- 	var _src = _this.attr('data-src');
- 	var _alt = _this.attr('data-alt');
-
+ 	var _this_i = _this.index();
+ 	// console.log(_this_i);
+ 	// console.log(modalData[_this_i]);
+ 	
+ 	var _src = url + modalData[_this_i].src;
+ 	var _alt = modalData[_this_i].alt;
  	modal_img.attr({'src':_src, 'alt':_alt});
 	ModalOn('modal_dp');
  });
