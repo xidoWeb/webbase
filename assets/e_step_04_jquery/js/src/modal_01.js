e733fc499de017f9269c93f74817d49142f19d07
xido <eriver2@gmail.com>
2017-08-18 11:18:19 +0900

    modal_01 step_01 이미지 변경처리 완료

--- assets/e_step_04_jquery/js/src/modal_01.js
+++ assets/e_step_04_jquery/js/src/modal_01.js
@@ -1,6 +1,12 @@
 //  modal_01.js
 
 (function($) {
+	var modalBox = $('.modal_box');
+	modalBox.html('<img>');
+	var modal_img = modalBox.find('img');
+	// modal_img.attr({'src':'../img/gallery/j01_big.jpg', 'alt':'큰이미지 내용'});
+
+
 // addClass 다수처리를 위한 함수생성
 	var ModalOff = function(display){	
 		$('.modal_box').addClass(display);
@@ -11,11 +17,29 @@
 		$('.modal_box').removeClass(display);
 		$('.modal_bg').removeClass(display);
 	};
+	// Modal창 사라지기
+	 ModalOff('modal_dp');
 
 // -------------------------------
  $('.modal_bg').on('click',function(){
 	 ModalOff('modal_dp');
  });
+// ------------------------------
+ var galleryBox = $('.gallery_list');
+ var gallery_list = galleryBox.children('li');
+
+// li클릭시 모달창 띄우기
+ gallery_list.on('click',function(e){
+ 	e.preventDefault();
+ 	var _this = $(this);
+ 	var _src = _this.attr('data-src');
+ 	var _alt = _this.attr('data-alt');
+
+ 	modal_img.attr({'src':_src, 'alt':_alt});
+	ModalOn('modal_dp');
+ });
+
+
 
 
 })(this.jQuery);
