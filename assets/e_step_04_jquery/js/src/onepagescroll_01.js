86a78f7a978e0f0ae28a7d0d4a90ce5ae747113d
xido <eriver2@gmail.com>
2017-08-29 10:33:43 +0900

    onepagescroll_01

---
+++ assets/e_step_04_jquery/js/src/onepagescroll_01.js
@@ -0,0 +1,13 @@
+// onepagescroll_01.js
+
+(function($) {
+	var nav = $('#gnb');
+	var nav_a = nav.find('a');
+
+	nav_a.on('click', function(e) {
+		e.preventDefault();
+
+		
+	});
+
+})(this.jQuery);
