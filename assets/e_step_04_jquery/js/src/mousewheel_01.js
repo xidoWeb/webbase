9987ed86508698b020442dbea952ac0d2cd92a72
xido <eriver2@gmail.com>
2017-08-30 10:50:28 +0900

    wheel기능을 통해 간단하게 class명추가/삭제 처리

--- assets/e_step_04_jquery/js/src/mousewheel_01.js
+++ assets/e_step_04_jquery/js/src/mousewheel_01.js
@@ -16,30 +16,39 @@
   	// console.log(event.originalEvent.wheelDeltaY);
 
   	// firefox event test
-  	console.log(E);
+  	// console.log(E);
 
   	// firefox DOMMouseScroll 기능에 따라 처리
   	if(E.detail){
   		delta = E.detail * -40;
-  		// console.log(delta);
   	}
 		// 기타 다른 브라우저
   	else{
   		delta = E.wheelDeltaY;
-  		// console.log(delta);
   	}
 
   	// 각 OS별 수치값이 다르기 때문에 동일한 수치로 조정
-  	if(delta <= 0){
-  		delta = -1;
-  	}else{
-  		delta = 1;
-  	}
+  	(delta <= 0) ? (delta = -1) : (delta = 1);
 
-  	console.log(delta);
+  	// console.log(delta);
+  	switch(delta){
+  		case -1:
+  			$('body').addClass('down').removeClass('up');
+  		break;
+  		
+  		case 1:
+  			$('body').addClass('up').removeClass('down');
+  		break;
+  	}
   };
+
  
 
 
 
 })(this.jQuery);
+
+
+
+
+
