d13466e49206aaa7610406a1fd35db702c4bd751
xido <eriver2@gmail.com>
2017-09-05 13:37:22 +0900

    slidebanner_03 버튼 클릭시 좌우무한 이동처리, 내용정리

--- assets/e_step_04_jquery/js/src/slideBanner_03.js
+++ assets/e_step_04_jquery/js/src/slideBanner_03.js
@@ -8,39 +8,29 @@
 	// 6. 마우스 올렸을경우(mouseenter) 자동 변경기능이 멈추고, 벗어날경우(mouseleave) 다시 변경되도록 처리
 
 
+
 // 좌, 우 버튼 일정 위치에서 사라지거나, 나타나게 만드는 기능을 함수화 처리
+
 	function BtnEnd(i){
-		// if(i <= 0){
-		// 	i=0;
-		// 	lBtn.fadeOut();
-		// 	rBtn.fadeIn();
-		// }else if(i >= banner_i-1){
-		// 	i=banner_i-1;
-		// 	rBtn.fadeOut();
-		// 	lBtn.fadeIn();
-		// }else{
-		// 	lBtn.fadeIn();
-		// 	rBtn.fadeIn();
-		// }
-		// console.log(i);
 		banner.stop().animate({marginLeft: -i * 100 + '%'});
 		// indi_li.eq(i).addClass('active').siblings().removeClass('active');
+		IndiI(i);
 	}
-
+	function IndiI(i) {
+		indi_li.eq(i).addClass('active').siblings().removeClass('active');
+	}
 	// indicator클릭시 해당하는 값이 이동
+	var slideBanner = $('#slideBanner');
 	var banner = $('#bannerBox');
 	var banner_child = banner.children();
 	var banner_i = banner_child.length;  //  복제하기 전의 갯수를 파악
-	// console.log(banner_i);
 
 	var lBtn = $('.l_btn');
 	var rBtn = $('.r_btn');
 	var i = 1;
 
-	// 변수값보다 함수호출이 먼저되면 undefined 또는 에러를 도출
-	BtnEnd(i);
 
-	// ----------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------
 	// 4번기능처리
 	// 마지막요소를 복제
 	var cloneBanner = banner_child.last().clone();
@@ -49,17 +39,16 @@
 
 	banner_child = banner.children();
 	banner_i = banner_child.length; // 복제후 갯수를 재확인
- 	// console.log(banner_i);
 
  	// banner의 크기를 복제한 갯수 * 100% 만큼 수정
- 	banner.css({'width': banner_i * 100 + '%'});
+ 	banner.css({'width': banner_i * 100 + '%','marginLeft':'-100%'});
  	// banner의 자식요소 또한 전체크기 / 갯수% 만큼으로 수정
  	banner_child.css({'width':100 / banner_i + '%'});
 
-
+// ========================================================================
  	// banner갯수와 동일하게 indicator 처리 및 생성
- 	var slideBanner = $('#slideBanner');
- 	slideBanner.append('<ol class="indicator"></ol>');
+ 	
+ 	banner.after('<ol class="indicator"></ol>');
  	var indi = $('.indicator');
  	// .indicator 내부에 li값 및 button생성
  	// 반복기능을 이용하여 여러개의 li 생성 정확한 값
@@ -72,42 +61,53 @@
 	 		           + j +'"><button type="button"><span class="me_hidden">'
 	 		           + banner_t +'</span></button></li>');
  	}
-
+ 	// indicator갯수가 늘어나면 가운데 배치되도록 처리
  	var indi_width = indi.width();
  	indi.css({marginLeft: -indi_width / 2 });
-
+ 	// indicator 자식요소 변수지정
 	var indi_li = indi.children();
-	
-	// ------------------------------------------------------------------------------------------
+	BtnEnd(i);
 
-// ------------ indicator 클릭
+// ========================================================================
+
+// indicator 클릭 ------------------------------------
 	indi_li.on('click',['button'],function(e) {
 		e.preventDefault();
 		var _this = $(this);
 		BtnEnd( _this.index() );
 	});
 
-	// 2. 좌,우 버튼을 클릭해서 배너 이동 처리
+// 좌,우 버튼을 클릭해서 배너 이동 처리 ----------------
 	rBtn.on('click',function() {
-		BtnEnd(++i);
+		i+=1;
+		if(i >= banner_i){
+			i = 1;
+			banner.css({'marginLeft':0}).animate({marginLeft: -100 + '%'});
+			// indi_li.eq(i).addClass('active').siblings().removeClass('active');
+			IndiI(i);
+		}else{
+			banner.stop().animate({marginLeft: -i * 100 + '%'});
+		}
+		// indi_li.eq(i).addClass('active').siblings().removeClass('active');
+		IndiI(i);
 	});
-
+// -----------------------------------------------
 	lBtn.on('click',function() {
-		BtnEnd(--i);
+		i-=1;
+		banner.stop().animate({marginLeft: -i * 100 + '%'},function() {
+			if(i <= 0){
+				i = banner_i-1;	
+				banner.css({'marginLeft': -i * 100 + '%'});
+				// indi_li.eq(i).addClass('active').siblings().removeClass('active');
+				IndiI(i);
+			}
+		});
+		// indi_li.eq(i).addClass('active').siblings().removeClass('active');
+		IndiI(i);
 	});
 
-
-
-
+// 최종 indicator 처음내용 숨기기 및 배너영역 처리
+indi_li.eq(0).hide();
+slideBanner.css({'overflow':'hidden'});
 
 })(this.jQuery);
-
-
-
-
-
-
-
-
-
-
