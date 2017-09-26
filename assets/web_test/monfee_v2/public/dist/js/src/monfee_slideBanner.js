f0f8951e559ab7ad4d559b838b7b79dd0dfa5e06
xido <eriver2@gmail.com>
2017-09-26 10:31:49 +0900

    monfee_V2 adBanner step_01

---
+++ assets/web_test/monfee_v2/public/dist/js/src/monfee_slideBanner.js
@@ -0,0 +1,22 @@
+// monfee_slideBanner.js
+(function($) {
+//----------------------------------------------------
+//  버튼
+	var lBtn = $('.l_btn');
+	var rBtn = $('.r_btn');
+// 설명영역
+	var explainWrap = $('.explain_wrap');
+	var explainList = explainWrap.children();
+// 인디케이트
+	var indicator = $('.indicator').children('ol');
+	var indicatorLi = indicator.children();
+// 배경 이미지
+	var bgBanner = $('.banner_wrap');
+	var bgBannerList = bgBanner.children();
+// 기타 설정
+// ---------------------------------------------------
+
+
+
+
+})(this.jQuery);
