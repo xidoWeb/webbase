8f61c0f5987eccfe80eebd69327f210082803cf7
xido <eriver2@gmail.com>
2017-09-26 19:07:17 +0900

    monfee_V2 addBanner

--- assets/web_test/monfee_v2/public/dist/js/src/monfee_slideBanner.js
+++ assets/web_test/monfee_v2/public/dist/js/src/monfee_slideBanner.js
@@ -1,27 +1,31 @@
 // monfee_slideBanner.js
 (function($) {
 //----------------------------------------------------
+  var adBanner            = $('#adBanner');
 //  버튼
-	var lBtn         = $('.l_btn');
-	var rBtn         = $('.r_btn');
+	var lBtn                = $('.l_btn');
+	var rBtn                = $('.r_btn');
 // 설명영역
-	var explainWrap  = $('.explain_wrap');
-	var explainList  = explainWrap.children();
+	var explain             = $('.explain_wrap');
+	var explainList         = explain.children();
+	var explainListCount    = explainList.length;
 // 인디케이트
-	var indicator    = $('.indicator').children('ol');
-	var indicatorLi  = indicator.children();
+	var indicator           = $('.indicator').children('ol');
+	var indicatorList       = indicator.children();
+	var indicatorLiWidth    = indicatorList.first().outerWidth(true);
+	var indicatorListCount  = indicatorList.length;
 // 배경 이미지
-  var adBanner     = $('#adBanner');
-	var bgBanner     = $('.banner_wrap');
-	var bgBannerList = bgBanner.children();
+	var bgBanner            = $('.banner_wrap');
+	var bgBannerList        = bgBanner.children();
+	var bgBannerListCount   = bgBannerList.length;
 // 기타 설정
-	var i            = 0;
-	var timed        = 7000;
+	var i                   = 0;
+	var timed               = 7000;
 	var autoMove;
 // ---------------------------------------------------
 
 // indicator 클릭 
-indicatorLi.on('click',function(e) {
+indicatorList.on('click',function(e) {
 	e.preventDefault() ;
 
 	var _this        = $(this);
@@ -31,23 +35,81 @@ indicatorLi.on('click',function(e) {
 	BannerAll(i);
 });
 // ---------------------------------------------------
+
+// 좌,우 클릭시 연속으로 띠형태로 취해서 무한히 도는 것 같은 형태
+
+// (해당하는 자식요소)의 (첫번째)를 (복제)하여 (부모요소의 마지막에 담기)
+explainList.first().clone().appendTo(explain);
+// indicatorList.first().clone().appendTo(indicator);
+// bgBannerList.first().clone().appendTo(bgBanner);
+
+explainList         = explain.children();
+explainListCount    += 1;
+indicatorListCount  += 1;
+// bgBannerListCount += 1;
+
+// 마지막요소 추가하여 크기값(css) 수정
+explain.css({width:explainListCount * 100 + '%'});
+explainList.css({width:(100 / explainListCount) + '%'});
+// -------------------------------------------------------
+// 배경이미지 정리
+bgBannerList.css({opacity:0});
+bgBanner.find('.active').css({opacity:1});
+// -------------------------------------------------------
 // 사용 기능 함수처리
 function BannerAll(index){
 	// indicator 선택 체크 
+	// console.log(i);
+	// console.log(explainListCount);
 
-	indicatorLi.eq(index).addClass('active');
-	indicatorLi.eq(index).siblings().removeClass('active');
+if(i < 0 ){
+	i = explainListCount - 1;
+	explain.css({marginLeft: -i * 100 +'%' },function(){})
+	       .stop(true,false).animate({marginLeft: -(explainListCount - 2) * 100 +'%' });
 
-	// 배경 이미지 나타나게 만들기
-	bgBannerList.eq(index).siblings().animate({opacity:0},function() {
-		bgBannerList.eq(index).addClass('active');
-		bgBannerList.eq(index).siblings().removeClass('active').css({opacity:1});
+
+	i= explainListCount - 2;
+	indicatorList.eq(i).addClass('active');
+	indicatorList.eq(i).siblings().removeClass('active');
+
+	BgBanner(i);
+
+}else if(i >= explainListCount -1){
+	i = explainListCount - 1;
+	explain.stop(true,false).animate({marginLeft: -i * 100 + '%' }, function() {  
+		explain.css({marginLeft:0});
 	});
+		i=0;
+		// console.log(i);
+		indicatorList.eq(i).addClass('active');
+		indicatorList.eq(i).siblings().removeClass('active');
 
-	// explain 이동처리
-	explainWrap.animate({marginLeft: index * -100 + '%'});
+		BgBanner(i);
+		
+
+
+
+	}else{
+		explain.stop(true,false).animate({marginLeft: -index * 100 + '%'});
+		indicatorList.eq(index).addClass('active');
+		indicatorList.eq(index).siblings().removeClass('active');
+		BgBanner(i);
+
+
+	}
+}
+
+// 배경이미지 설정
+function BgBanner(i){
+	bgBannerList.eq(i).css({opacity:1});
+	bgBanner.find('.active').stop(true,false).animate({opacity:0},function() {
+		bgBannerList.eq(i).addClass('active');
+		bgBannerList.eq(i).siblings().removeClass('active').css({opacity:0});
+	});
 }
 // ---------------------------------------------------
+// setInterval, clearInterval
+
 
 // setInterval(반복실행함수, 시간)  : 일정 시간마다 반복 처리하는 기능
 // clearInterval(setInterval변수명) : setInterval기능을 상쇄시키는 기능
@@ -61,18 +123,15 @@ function BannerAll(index){
 // setInterval을 구동할 수 없다.
 
 // -----------------------------------------------------
+// 일정 시간마다 움직이게하기, 마우스올리면 멈추기
 function SetIn(){
 	autoMove = setInterval( 
 		function(){ 
 			++i;
-			if(i >= 5){
-				i = 0;
-			}
-			console.log(i);
+			// if(i >= 5){	i = 0; }
 			BannerAll(i); 
-		} , timed);
-	
-}
+		} , timed);	
+	}
 
 function ClearIn(){
 	// autoMove = clearInterval(); 
@@ -82,18 +141,6 @@ function ClearIn(){
 SetIn();
 
 adBanner.on({'mouseenter': ClearIn,'mouseleave':SetIn });
-// -------------------------------------------------------
-// 좌,우 클릭시 연속으로 띠형태로 취해서 무한히 도는 것 같은 형태
-
-// // (해당하는 자식요소)의 (첫번째)를 (복제)하여 (부모요소의 마지막에 담기)
-// explainList.first().clone().appendTo(explainWrap);
-// indicatorLi.first().clone().appendTo(indicator);
-// bgBannerList.first().clone().appendTo(bgBanner);
-
-// // 마지막요소 추가하여 크기값(css) 수정
-// explainWrap.css({width:600+'%'});
-// explainList.css({width:100 / 6 +'%'});
-// indicator.css({width:330 + 'px',width:330 / 16 + 'rem'});
 
 
 // -------------------------------------------------------
@@ -102,8 +149,7 @@ adBanner.on({'mouseenter': ClearIn,'mouseleave':SetIn });
 lBtn.on('click',function(e) {
 	e.preventDefault();
 	// console.log(i);
-	--i;
-	if(i < 0){ i = 4 }
+	i-=1;
 	BannerAll(i);
 });
 // -------------------------------------------------------
@@ -111,11 +157,18 @@ lBtn.on('click',function(e) {
 
 rBtn.on('click',function(e){
 	e.preventDefault();
-	++i;
-	if(i >= 5){ i = 0 }
+	i+=1;
 	BannerAll(i);
 });
 // -------------------------------------------------------
+
+$('h1').on('mouseenter', function() {
+	$(this).animate({marginTop:'20px'}, function(){
+		$(this).animate({marginLeft:'20px'}, function(){
+			$(this).css({margin:0});
+		});
+	});
+});
 
 })(this.jQuery);
 
