3ebcfc04c04d68ef053d453d270d14a9a414d95b
xido <eriver2@gmail.com>
2017-09-26 10:53:39 +0900

    monfee_V2e adBanner indicator

--- assets/web_test/monfee_v2/public/dist/js/src/monfee_slideBanner.js
+++ assets/web_test/monfee_v2/public/dist/js/src/monfee_slideBanner.js
@@ -16,7 +16,47 @@
 // 기타 설정
 // ---------------------------------------------------
 
+// indicator 클릭 
+indicatorLi.on('click',function(e) {
+	e.preventDefault() ;
+
+	var _this = $(this);
+	var index = _this.index();
+	// console.log(index);
+
+	// indicator 선택 체크 
+	indicatorLi.eq(index).addClass('active');
+	indicatorLi.eq(index).siblings().removeClass('active');
+
+	// 배경 이미지 나타나게 만들기
+	bgBannerList.eq(index).addClass('active');
+	bgBannerList.eq(index).siblings().removeClass('active');
+
+	// explain 이동처리
+	explainWrap.animate({marginLeft: index * -100 + '%'});
+
+});
+
 
 
 
 })(this.jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
